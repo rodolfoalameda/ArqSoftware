10ba4370a5092ba0789c4ca9ef795c53940fbf97
Rodolfo <rodolfoalmeida_a@hotmail.com>
2020-10-21 17:45:18 -0300

    commit

--- JDBC/projetojdbc/src/main/java/com/jdbcexample/proejtojdbc/ProjetojdbcApplication.java
+++ JDBC/projetojdbc/src/main/java/com/jdbcexample/proejtojdbc/ProjetojdbcApplication.java
@@ -1,35 +1,13 @@
 package com.jdbcexample.proejtojdbc;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import db.DB;
+import com.jdbcexample.proejtojdbc.entities.Departamento;
 
 public class ProjetojdbcApplication {
 
 	public static void main(String[] args) {
 
-		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
-
-		//FindAll de uma tabela;
-		try {
-			conn = DB.getConnection();
-			st = conn.createStatement();
-			rs = st.executeQuery("select * from aluno");
-			while (rs.next()) {
-				System.out.println(rs.getInt("id") + ", " + rs.getString("nome"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			DB.closeResultSet(rs);
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+		Departamento d1 = new Departamento(1, "Games");
+		System.out.println(d1);
 	}
 
 }
---
+++ JDBC/projetojdbc/src/main/java/com/jdbcexample/proejtojdbc/entities/Departamento.java
@@ -0,0 +1,63 @@
+package com.jdbcexample.proejtojdbc.entities;
+
+import java.io.Serializable;
+
+public class Departamento implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String nome;
+
+	public Departamento() {
+
+	}
+
+	public Departamento(int id, String nome) {
+		
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Departamento))
+			return false;
+		Departamento other = (Departamento) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Departamento [id=" + id + ", nome=" + nome + "]";
+	}
+
+}
