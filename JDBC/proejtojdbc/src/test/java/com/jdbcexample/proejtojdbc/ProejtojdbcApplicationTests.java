a935299fe7b4dbff53a338508070fdeb3b2fe413
Rodolfo <rodolfoalmeida_a@hotmail.com>
2020-10-21 16:19:03 -0300

    commit

---
+++ JDBC/proejtojdbc/src/test/java/com/jdbcexample/proejtojdbc/ProejtojdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdbcexample.proejtojdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProejtojdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
